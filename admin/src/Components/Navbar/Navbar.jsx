e85d97effc6ea0f36979c2af099bac6705da1b0a
kakoii <jaycehollon@gmail.com>
2024-03-26 14:25:42 -0500

    Admin Panel Add/List of Products

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/logo.png'
-import navProfile from '../../assets/nav_dropdown.png'
+import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
     <div className ='navbar'>
       <img src={navlogo} alt="" className="nav-logo" />
-      <img src={navProfile} alt="" className="navprofile" />
+      <img src={navProfile} alt="" className="nav-profile" />
     </div>
   )
 }
